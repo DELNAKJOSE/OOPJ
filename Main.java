952cce0bff47e893ffb13adc0f97385c0b2e6fd1
DELNAKJOSE <86092162+DELNAKJOSE@users.noreply.github.com>
2021-12-06 22:49:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+//________________________AUTHOR:DELNA K. JOSE________________________
+//___________________________DATE:06-12-2021__________________________
+//__________________TO UNDERSTAND THE BASIC SYNTAXES__________________
+//_______________________MY FIRST JAVA PROGRAM________________________
+
+package package1;                                           //PACKAGE IN WHICH MAIN CLASS BELONGS TO
+
+public class Main {                                         //MAIN CLASS
+
+	public static void main(String[] args) {                //MAIN METHOD (ALWAYS STATIC)
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");                               //CALLING OR EXECUTING PRINTLINE 
+
+	}
+
+}
